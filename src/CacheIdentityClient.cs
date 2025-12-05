36704caf6ed0bc5f774b852bf2e66f8b142b4793
Per Stolpe <per.stolpe@walley.se>
2025-12-05 10:25:40 +0100

    allow for synchronous prefetching of tokencredentials

--- src/CacheIdentityClient.cs
+++ src/CacheIdentityClient.cs
@@ -18,19 +18,54 @@ internal interface ICacheIdentityClient
     /// <summary>
     /// Acquire a token to be used to authenticate a connection.
     /// </summary>
+    /// <returns>A TokenResult containing a token and expiration if synchronous token retrieval is supported.</returns>
+    TokenResult GetToken(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Acquire a token to be used to authenticate a connection.
+    /// </summary>
     /// <returns>A TokenResult containing a token and expiration</returns>
     Task<TokenResult> GetTokenAsync(CancellationToken cancellationToken);
 }
 
 internal class CacheIdentityClient : ICacheIdentityClient
 {
-    private readonly Func<CancellationToken, Task<TokenResult>> _getToken;
+    private readonly Func<CancellationToken, TokenResult>? _getToken;
+    private readonly Func<CancellationToken, Task<TokenResult>> _getTokenAsync;
 
-    private CacheIdentityClient(Func<CancellationToken, ValueTask<AccessToken>> getToken)
-        => _getToken = async (cancellationToken) => new TokenResult(await getToken.Invoke(cancellationToken).ConfigureAwait(false));
+    private CacheIdentityClient(
+            Func<CancellationToken, ValueTask<AccessToken>> getTokenAsync)
+        : this(
+              getToken: null,
+              getTokenAsync)
+    { }
 
-    private CacheIdentityClient(Func<CancellationToken, Task<AuthenticationResult>> getToken)
-        => _getToken = async (cancellationToken) => new TokenResult(await getToken.Invoke(cancellationToken).ConfigureAwait(false));
+    private CacheIdentityClient(
+        Func<CancellationToken, AccessToken>? getToken,
+        Func<CancellationToken, ValueTask<AccessToken>> getTokenAsync)
+    {
+        _getToken = getToken is not null ?
+            cancellationToken => new TokenResult(getToken(cancellationToken))
+            : null;
+        _getTokenAsync = async (cancellationToken) => new TokenResult(await getTokenAsync.Invoke(cancellationToken).ConfigureAwait(false));
+    }
+
+    private CacheIdentityClient(
+            Func<CancellationToken, Task<AuthenticationResult>> getTokenAsync)
+        : this(
+              getToken: null,
+              getTokenAsync)
+    { }
+
+    private CacheIdentityClient(
+        Func<CancellationToken, AuthenticationResult>? getToken,
+        Func<CancellationToken, Task<AuthenticationResult>> getTokenAsync)
+    {
+        _getToken = getToken is not null ?
+            (cancellationToken) => new TokenResult(getToken(cancellationToken))
+            : null;
+        _getTokenAsync = async (cancellationToken) => new TokenResult(await getTokenAsync.Invoke(cancellationToken).ConfigureAwait(false));
+    }
 
     internal static ICacheIdentityClient CreateForManagedIdentity(AzureCacheOptions options)
     {
@@ -42,7 +77,7 @@ internal class CacheIdentityClient : ICacheIdentityClient
                           : ManagedIdentityId.WithUserAssignedResourceId(options.ClientId))
                   .Build();
 
-        return new CacheIdentityClient(getToken: (cancellationToken) => clientApp.AcquireTokenForManagedIdentity(options.Scope).ExecuteAsync(cancellationToken));
+        return new CacheIdentityClient(getTokenAsync: (cancellationToken) => clientApp.AcquireTokenForManagedIdentity(options.Scope).ExecuteAsync(cancellationToken));
     }
 
     internal static ICacheIdentityClient CreateForServicePrincipal(AzureCacheOptions options)
@@ -52,18 +87,32 @@ internal class CacheIdentityClient : ICacheIdentityClient
             .WithCredentials(options)
             .Build();
 
-        return new CacheIdentityClient(getToken: (cancellationToken) => clientApp.AcquireTokenForClient(new[] { options.Scope }).ExecuteAsync(cancellationToken));
+        return new CacheIdentityClient(getTokenAsync: (cancellationToken) => clientApp.AcquireTokenForClient(new[] { options.Scope }).ExecuteAsync(cancellationToken));
     }
 
     internal static ICacheIdentityClient CreateForTokenCredential(TokenCredential tokenCredential, string scope)
     {
         var tokenRequestContext = new TokenRequestContext(new[] { scope });
 
-        return new CacheIdentityClient(getToken: (cancellationToken) => tokenCredential.GetTokenAsync(tokenRequestContext, cancellationToken));
+        return new CacheIdentityClient(
+            getToken: (cancellationToken) => tokenCredential.GetToken(tokenRequestContext, cancellationToken),
+            getTokenAsync: (cancellationToken) => tokenCredential.GetTokenAsync(tokenRequestContext, cancellationToken));
+    }
+
+    TokenResult ICacheIdentityClient.GetToken(CancellationToken cancellationToken)
+    {
+        if (_getToken is not null)
+        {
+            return _getToken.Invoke(cancellationToken);
+        }
+        else
+        {
+            throw new InvalidOperationException("Synchronous token fetch not supported");
+        }
     }
 
     async Task<TokenResult> ICacheIdentityClient.GetTokenAsync(CancellationToken cancellationToken)
-        => await _getToken.Invoke(cancellationToken).ConfigureAwait(false);
+        => await _getTokenAsync.Invoke(cancellationToken).ConfigureAwait(false);
 
 }
 
